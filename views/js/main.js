e3657b1daaa2e7470271dd789a09e2b32be6fa6b
Lei Zhang <leiz2010@gmail.com>
2015-01-29 00:32:42 -0800

    requestAnimationFrame for changePizzaSizes

--- views/js/main.js
+++ views/js/main.js
@@ -450,6 +450,7 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    requestAnimationFrame(changePizzaSizes);
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
